e8a6521445374eb783782327bcfdfd6d456b7fab
mambax7 <mambax7@gmail.com>
2020-07-11 22:44:01 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include("../../mainfile.php");
+$xoopsOption['template_main'] = "all_your_base.html";
+include(XOOPS_ROOT_PATH."/header.php");
+include("header.php");
+
+$xoopsOption['show_rblock'] = 1;
+ ?>
+<html>
+<body>
+<p>
+<a href="http://www.allyourbasearebelongtous.com/">All Your Base Are Belong To Us</a>
+<p>
+<a href="http://shadowsbastion.com/">Ported to XOOPS by Shadow's Bastion</a>
+</body>
+</html>
+
+
+<?php
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "all_your_base";
+$modversion['version'] = 1.00;
+$modversion['description'] = "Flash games hosted on your site.";
+$modversion['author'] = "Ported To Xoops By Shadow (<a href='http://shadowsbastion.com/'>Shadow's Bastion</a>)";
+$modversion['credits'] = "All Your Base Are Belong To Us(<a href='http://www.allyourbasearebelongtous.com/'>All Your Base Are Belong To Us</a>)";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/all_your_base.jpg";
+$modversion['dirname'] = "all_your_base";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "all_your_base.html";
+$modversion['templates'][1]['description'] = "The template file of index";
+?>
